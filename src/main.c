2fde3f3833691e9c2a3f0ee141bcdf113def0e7c
candy06 <loic.rose1995@gmail.com>
2018-05-10 14:55:34 +0200

    Clean up of code + add test files.

--- src/main.c
+++ src/main.c
@@ -3,24 +3,45 @@
 
 #define ROOT 0
 
-void broadcast_ring(void * data, int count, MPI_Datatype datatype, int root, MPI_Comm communicator) {
+int current_process_rank, number_of_processes,
+  next_to_src, prev_of_src, next, prev, result, tmp;
 
-  int number_of_processes, current_process_rank;
-  MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
-  MPI_Comm_rank(MPI_COMM_WORLD, &current_process_rank);
-
-  int next_to_src = (ROOT + 1) % number_of_processes;
-  int prev_of_src = (ROOT + number_of_processes - 1) % number_of_processes;
-  int next = (current_process_rank + 1) % number_of_processes;
-  int prev = (current_process_rank - 1 + number_of_processes) % number_of_processes;
+void broadcast_ring(int * data, int count, MPI_Datatype datatype, MPI_Comm communicator) {
 
   if (current_process_rank == ROOT) {
-    MPI_Send(data, 1, MPI_INT, next_to_src, 0, MPI_COMM_WORLD);
+    MPI_Send(data, count, MPI_INT, next_to_src, 0, MPI_COMM_WORLD);
   } else if (current_process_rank == prev_of_src) {
-    MPI_Recv(data, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(data, count, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   } else {
-    MPI_Recv(data, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Send(data, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
+    MPI_Recv(data, count, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Send(data, count, MPI_INT, next, 0, MPI_COMM_WORLD);
+  }
+
+}
+
+void scatter_ring(int * data, int count, MPI_Datatype datatype, MPI_Comm communicator) {
+
+  if (current_process_rank == ROOT) {
+    for (int i = 1; i < 4; i++) {
+      MPI_Send(&data[(ROOT - i + number_of_processes) % number_of_processes], count, MPI_INT, next_to_src, 0, MPI_COMM_WORLD);
+    }
+    result = data[current_process_rank];
+  } else {
+
+    MPI_Recv(&tmp, count, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    for (int i = 0; i < (ROOT - 1 - current_process_rank + number_of_processes) % number_of_processes; i++) {
+      MPI_Send(&tmp, count, MPI_INT, next, 0, MPI_COMM_WORLD);
+      MPI_Recv(&tmp, count, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    result = tmp;
+  }
+
+}
+
+void displayVector(int * vector, int sizeOfVector, int processID) {
+  printf("ProcessID: %d - ", processID);
+  for (int i = 0; i < sizeOfVector; i++) {
+    (i == sizeOfVector - 1) ? printf("%d\n", vector[i]) : printf("%d ", vector[i]);
   }
 }
 
@@ -28,43 +49,33 @@ int main(int argc, char const *argv[]) {
 
   MPI_Init(NULL, NULL);
 
-  int number_of_processes, current_process_rank;
   MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
   MPI_Comm_rank(MPI_COMM_WORLD, &current_process_rank);
+  next_to_src = (ROOT + 1) % number_of_processes;
+  prev_of_src = (ROOT + number_of_processes - 1) % number_of_processes;
+  next = (current_process_rank + 1) % number_of_processes;
+  prev = (current_process_rank - 1 + number_of_processes) % number_of_processes;
 
-  int next_to_src = (ROOT + 1) % number_of_processes;
-  int prev_of_src = (ROOT + number_of_processes - 1) % number_of_processes;
-  int next = (current_process_rank + 1) % number_of_processes;
-  int prev = (current_process_rank - 1 + number_of_processes) % number_of_processes;
-
-  int data[4] = {10, 20, 30, 40};
-  int result;
-  int tmp;
+  // Create a vector
+  int vector[4] = {0};
+  int matrix[4] = {0};
   if (current_process_rank == ROOT) {
-    for (int i = 1; i < 4; i++) {
-      MPI_Send(&data[(ROOT - i + number_of_processes) % number_of_processes], 1, MPI_INT, next_to_src, 0, MPI_COMM_WORLD);
-    }
-    result = data[0];
-  } else {
-
-    MPI_Recv(&tmp, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    for (int i = 0; i < (ROOT - 1 - current_process_rank + number_of_processes) % number_of_processes; i++) {
-      MPI_Send(&tmp, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
-      MPI_Recv(&tmp, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    result = tmp;
+    vector[0] = 10;
+    vector[1] = 20;
+    vector[2] = 30;
+    vector[3] = 40;
+    matrix[0] = 100;
+    matrix[1] = 200;
+    matrix[2] = 300;
+    matrix[3] = 400;
   }
 
-  printf("Hello from process %d out of %d, my data is %d\n", current_process_rank, number_of_processes, result);
+  broadcast_ring(vector, 4, MPI_INT, MPI_COMM_WORLD);
 
+  scatter_ring(matrix, 1, MPI_INT, MPI_COMM_WORLD);
 
-  // int data;
-  // if (current_process_rank == ROOT) {
-  //   data = 100;
-  // }
-  //
-  // broadcast_ring(&data, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
-  // printf("Hi am process %d out of %d and the data is %d.\n", current_process_rank, number_of_processes, data);
+  displayVector(vector, 4, current_process_rank);
+  printf("Hi am process %d out of %d and the data is %d.\n", current_process_rank, number_of_processes, result);
 
   MPI_Finalize();
 
